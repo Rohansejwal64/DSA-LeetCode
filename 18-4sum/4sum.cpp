3128b0ff7506765e19340ef3eeb2f49bc051d269
Rohansejwal64 <137511240+Rohansejwal64@users.noreply.github.com>
2025-06-07 01:35:55 +0530

    Time: 21 ms (66.26%) | Memory: 17.5 MB (31.03%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,30 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-    int n = nums.size(); //size of the array
-    set<vector<int>> st;
-
-    //checking all possible quadruplets:
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            set<long long> hashset;
-            for (int k = j + 1; k < n; k++) {
-                // taking bigger data type
-                // to avoid integer overflow:
-                long long sum = nums[i] + nums[j];
-                sum += nums[k];
-                long long fourth = target - sum;
-                if (hashset.find(fourth) != hashset.end()) {
-                    vector<int> temp = {nums[i], nums[j], nums[k], (int)(fourth)};
-                    sort(temp.begin(), temp.end());
-                    st.insert(temp);
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<n;j++){
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
+                int k=j+1;
+                int l=n-1;
+                while(k<l){
+                    long long sum=nums[i]+nums[j];
+                    sum+=nums[k];
+                    sum+=nums[l];
+                    if(sum==target) {
+                        vector<int> temp={nums[i],nums[j],nums[k],nums[l]};
+                        ans.push_back(temp);
+                        k++;
+                        l--;
+                        while(k<l && nums[l]==nums[l+1]) l--;
+                        while(k<l && nums[k]==nums[k-1]) k++;
+                        
+                    }
+                    else if(sum>target){
+                        l--;
+                        
+                    }
+                    else{
+                        k++;
+                    }
                 }
-                // put the kth element into the hashset:
-                hashset.insert(nums[k]);
             }
+
         }
+        return ans;
+        
     }
-    vector<vector<int>> ans(st.begin(), st.end());
-    return ans;
-}
 };
