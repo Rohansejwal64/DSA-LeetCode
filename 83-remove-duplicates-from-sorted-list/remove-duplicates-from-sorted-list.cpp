c46ac17198655e917252f317a4a89c2a57aa0c3d
Rohansejwal64 <137511240+Rohansejwal64@users.noreply.github.com>
2025-06-23 23:30:56 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (34.98%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
@@ -11,18 +11,20 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        if(head==NULL) return NULL;
-        ListNode* temp=head;
-        while(temp!=NULL && temp->next!=NULL){
-            while(temp->next!=NULL && temp->val == temp->next->val){
-                ListNode* dupli=temp->next;
-                temp->next=dupli->next;
-                dupli->next=NULL;
-                delete dupli;
-            }
-            temp=temp->next;
+    if (head == NULL) return NULL;
+
+    ListNode* curr = head;
+    while (curr != NULL && curr->next != NULL) {
+        if (curr->val == curr->next->val) {
+            ListNode* dupe = curr->next;
+            curr->next = dupe->next;
+            delete dupe;  // free memory
+        } else {
+            curr = curr->next;
         }
-        return head;
-        
     }
+
+    return head;
+}
+
 };
