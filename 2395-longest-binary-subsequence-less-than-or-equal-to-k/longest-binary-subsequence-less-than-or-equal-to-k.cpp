78804aca124fde1de677f35eab6015ae56724003
Rohansejwal64 <137511240+Rohansejwal64@users.noreply.github.com>
2025-06-26 11:27:01 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (73.55%) - LeetSync

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int longestSubsequence(string s, int k) {
-      int sum = 0, cnt = 0, n = s.size();
-for (int j = n - 1, bit = 0; j >= 0; --j) {
-    if (s[j] == '0') {
-        cnt++;
-    }
-    else if (bit < 31 && sum + (1 << bit) <= k) {
-        sum += (1 << bit);
-        cnt++;
-    }
-    bit++;
-}
-return cnt;
-
-
-        
+        int sum = 0, cnt = 0, n = s.size();
+        for (int j = n - 1, bit = 0; j >= 0; --j) {
+            if (s[j] == '0') {
+                cnt++;
+            } else if (bit < 31 && sum + (1 << bit) <= k) {
+                sum += (1 << bit);
+                cnt++;
+            }
+            bit++;
+        }
+        return cnt;
     }
 };
