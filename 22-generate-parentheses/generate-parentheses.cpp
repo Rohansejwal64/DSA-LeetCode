fc50341ccdb496284d46509256879b132150c676
Rohansejwal64 <137511240+Rohansejwal64@users.noreply.github.com>
2025-07-26 22:29:04 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (88.84%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-void solve(int &n,string temp, vector<string> &ans,int open,int close){
-    if(open==n && close==n){
+void solve(int n, string &temp, vector<string> &ans, int open, int close) {
+    if (open == n && close == n) {
         ans.push_back(temp);
         return;
     }
-    if(open<n){
-        solve(n,temp+'(' , ans, open+1,close);
+    
+    if (open < n) {
+        temp.push_back('(');
+        solve(n, temp, ans, open + 1, close);
+        temp.pop_back();  // undo
     }
-    if(open>close){
-        solve(n,temp+')', ans, open, close+1);
+    
+    if (close < open) {
+        temp.push_back(')');
+        solve(n, temp, ans, open, close + 1);
+        temp.pop_back();  // undo
     }
 }
-    vector<string> generateParenthesis(int n) {
-        vector<string> ans;
-        int open=0;
-        int close=0;
-        solve(n,"",ans,open,close);
-        return ans;
-        
-    }
+
+vector<string> generateParenthesis(int n) {
+    vector<string> ans;
+    string temp;
+    solve(n, temp, ans, 0, 0);
+    return ans;
+}
+
 };
