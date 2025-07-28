c148df7f58fdb0d0216ab1ab1e4e303983c9f8fa
Rohansejwal64 <137511240+Rohansejwal64@users.noreply.github.com>
2025-07-28 23:27:27 +0530

    Time: 15 ms (14.00%) | Memory: 24.1 MB (10.37%) - LeetSync

---
+++ 51-n-queens/n-queens.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+bool issafe(int n,int row, int col, vector<string> temp){
+    int r=row,c=col;
+    while(r>=0 && c>=0){
+        if(temp[r][c]=='Q') return false;
+        r--,c--;
+
+    }
+     r=row,c=col;
+    while(r<n && c>=0){
+        if(temp[r][c]=='Q') return false;
+        r++,c--;
+
+    }
+
+    return true;
+    
+}
+void solve(int &n,vector<vector<string>> &ans,vector<string> &temp,vector<bool> &rowvis,int c){
+    
+    if(c==n) {
+        ans.push_back(temp);
+        return;
+    }
+    for(int r=0;r<n;r++){
+        if(!rowvis[r] && issafe(n,r,c,temp)){
+            temp[r][c]='Q';
+            rowvis[r]=true;
+            solve(n,ans,temp,rowvis,c+1);
+            temp[r][c]='.';
+            rowvis[r]=false;
+
+        }
+    }
+}
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> temp(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++){
+            temp[i]=s;
+        }
+
+        vector<bool> rowvis(n,false);
+        solve(n,ans,temp,rowvis,0);
+        return ans;
+
+        
+    }
+};
