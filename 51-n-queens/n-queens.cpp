f9e11b1624b89c83e6faf223a500cf3a10ebd61f
Rohansejwal64 <137511240+Rohansejwal64@users.noreply.github.com>
2025-07-28 23:41:08 +0530

    Time: 2 ms (75.44%) | Memory: 10 MB (92.78%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -17,19 +17,24 @@ bool issafe(int row, int col, vector<string> &temp){
     return true;
     
 }
-void solve(int &n,vector<vector<string>> &ans,vector<string> &temp,vector<bool> &rowvis,int c){
+void solve(int &n,vector<vector<string>> &ans,vector<string> &temp,vector<bool> &rowvis,vector<bool> &diagvis1,vector<bool> &diagvis2,int c){
     
     if(c==n) {
         ans.push_back(temp);
         return;
     }
     for(int r=0;r<n;r++){
-        if(!rowvis[r] && issafe(r,c,temp)){
+        if(!rowvis[r] && !diagvis1[r+c] && !diagvis2[n-1+c-r]){
             temp[r][c]='Q';
             rowvis[r]=true;
-            solve(n,ans,temp,rowvis,c+1);
+            diagvis1[r+c]=true;
+            diagvis2[n-1+c-r]=true;
+            solve(n,ans,temp,rowvis,diagvis1,diagvis2,c+1);
             temp[r][c]='.';
             rowvis[r]=false;
+            diagvis1[r+c]=false;
+            diagvis2[n-1+c-r]=false;
+
 
         }
     }
@@ -43,7 +48,10 @@ void solve(int &n,vector<vector<string>> &ans,vector<string> &temp,vector<bool> 
         }
 
         vector<bool> rowvis(n,false);
-        solve(n,ans,temp,rowvis,0);
+        vector<bool> diagvis1(2*n-1,false);
+        vector<bool> diagvis2(2*n-1,false);
+
+        solve(n,ans,temp,rowvis,diagvis1,diagvis2,0);
         return ans;
 
         
