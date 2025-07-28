262b97c488b657780f8654d83a9e34640e6c3e04
Rohansejwal64 <137511240+Rohansejwal64@users.noreply.github.com>
2025-07-28 13:49:25 +0530

    Time: 29 ms (92.40%) | Memory: 53.5 MB (75.46%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-bool ispalin(string s){
+bool ispalin(string &s){
     int i=0;
     int j=s.size()-1;
     while(i<=j){
@@ -11,7 +11,7 @@ bool ispalin(string s){
     return true;
 
 }
-void solve(string s,vector<vector<string>> &ans,vector<string> &temp,int index){
+void solve(string &s,vector<vector<string>> &ans,vector<string> &temp,int index){
     if(index==s.size()){
         ans.push_back(temp);
         return;
