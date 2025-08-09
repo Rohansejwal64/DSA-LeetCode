b56f048d1203d0fc7f1441af8a949cd044d70dd0
Rohansejwal64 <137511240+Rohansejwal64@users.noreply.github.com>
2025-08-09 16:50:52 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (58.55%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
 
-bool dfs(vector<vector<int>> &adj,vector<bool> &visited,vector<bool> &inrecursion,int u){
-    visited[u]=true;
-    inrecursion[u]=true;
-    for(int &v:adj[u]){
-        if(visited[v] && inrecursion[v]) return true;
-        if(!visited[v] && dfs(adj, visited, inrecursion, v)){
-            return true;
-        }
+bool dfs(vector<vector<int>> &adj, vector<bool> &visited, vector<bool> &inrecursion, int u) {
+    visited[u] = true;
+    inrecursion[u] = true;
+    for (int v : adj[u]) {
+        if (visited[v] && inrecursion[v]) return true; // cycle found
+        if (!visited[v] && dfs(adj, visited, inrecursion, v)) return true;
     }
-    inrecursion[u]=false;
+    inrecursion[u] = false;
     return false;
 }
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
-        for(vector<int> &vec: prerequisites){
-            adj[vec[1]].push_back(vec[0]);
-        }
-        vector<bool> visited(numCourses,false);
-        vector<bool> inrecursion(numCourses,false);
-        for(int i=0;i<numCourses;i++){
-            if(!visited[i] && dfs(adj,visited,inrecursion,i)){
-                return false;
-            }
-        }
-        return true;
-        
+
+bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+    vector<vector<int>> adj(numCourses);
+    for (auto &vec : prerequisites) {
+        adj[vec[1]].push_back(vec[0]);
     }
+    vector<bool> visited(numCourses, false);
+    vector<bool> inrecursion(numCourses, false);
+
+    for (int i = 0; i < numCourses; i++) {
+        if (!visited[i] && dfs(adj, visited, inrecursion, i)) {
+            return false; // cycle found
+        }
+    }
+    return true; // no cycle
+}
+
 };
