734ca2a464259d1eae24ac36e0c0ea71579352b0
Rohansejwal64 <137511240+Rohansejwal64@users.noreply.github.com>
2025-08-10 23:52:37 +0530

    Time: 273 ms (5.01%) | Memory: 110.7 MB (5.02%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+
+
+void dfs(vector<vector<int>>& board,vector<vector<bool>> &visited,int x, int y){
+    visited[x][y]=true;
+    vector<vector<int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+     for (vector<int>& d : directions) {
+            int nx = x + d[0];
+            int ny = y + d[1];
+            if (nx >= 0 && nx < board.size() && ny >= 0 &&
+                ny < board[0].size() && !visited[nx][ny] &&
+                board[nx][ny] == 1) {
+                    dfs(board,visited,nx,ny);
+            }
+        }
+
+
+
+
+}
+    int numEnclaves(vector<vector<int>>& grid) {
+        int r=grid.size();
+        int c=grid[0].size();
+        vector<vector<bool>> visited(r,vector<bool>(c,false));
+        for(int i=0;i<c;i++){
+            if(grid[0][i]==1 && !visited[0][i]){
+                dfs(grid,visited,0,i);
+            }
+        }
+        for(int i=0;i<c;i++){
+            if(grid[r-1][i]==1 && !visited[r-1][i]){
+                dfs(grid,visited,r-1,i);
+            }
+        }
+        for(int i=0;i<r;i++){
+            if(grid[i][0]==1 && !visited[i][0]){
+                dfs(grid,visited,i,0);
+            }
+        }
+        for(int i=0;i<r;i++){
+            if(grid[i][c-1]==1 && !visited[i][c-1]){
+                dfs(grid,visited,i,c-1);
+            }
+        }
+        int count=0;
+
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j]==1 && !visited[i][j]) count++;
+            }
+        }
+        return count;
+
+        
+    }
+};
