72aaac11f82673c823cd5b71816446ca78ab62d8
Rohansejwal64 <137511240+Rohansejwal64@users.noreply.github.com>
2025-08-11 00:55:20 +0530

    Time: 81 ms (46.25%) | Memory: 21.3 MB (45.66%) - LeetSync

---
+++ 127-word-ladder/word-ladder.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        if(beginWord==endWord) return 0;
+        unordered_set<string> s;
+        for(auto &st:wordList){
+            s.insert(st);
+        }
+        int count=0;
+        bool find=false;
+        queue<pair<string,int>> q;
+        q.push({beginWord,1});
+        count++;
+        if(s.find(beginWord)!=s.end()) {
+            s.erase(beginWord);
+        }
+        while(!q.empty()){
+            
+            auto it =q.front();
+            q.pop();
+            string temp=it.first;
+            if(temp==endWord){
+                count=it.second;
+                find=true;
+                break;
+            }
+            for(int i=0;i<temp.size();i++){
+                for(char ch='a';ch<='z';ch++){
+                    string temp2=temp;
+                    temp2[i]=ch;
+                    if(s.find(temp2)!=s.end()){
+                        q.push({temp2,it.second+1});
+                        s.erase(temp2);
+                    }
+                }
+            }
+            
+        }
+        return (find)?count:0;
+        
+
+    
+        
+    }
+};
