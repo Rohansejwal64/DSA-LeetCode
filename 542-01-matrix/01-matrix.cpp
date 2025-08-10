0d1e2c1aadc4b88bf875a29dd10d7dca76be644d
Rohansejwal64 <137511240+Rohansejwal64@users.noreply.github.com>
2025-08-10 20:08:14 +0530

    Time: 14 ms (78.13%) | Memory: 34.7 MB (79.38%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,41 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int r=mat.size();
-        int c=mat[0].size();
-        queue<pair<pair<int,int>,int>>q;
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(mat[i][j]==0){
-                    q.push({{i,j},0});
+    int r = mat.size();
+    int c = mat[0].size();
+    queue<pair<int,int>> q;
+    vector<vector<int>> ans(r, vector<int>(c, -1));
+    vector<vector<int>> directions = {{-1,0},{1,0},{0,-1},{0,1}};
 
-                }
+    // Initialize with all 0 cells
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            if(mat[i][j] == 0) {
+                ans[i][j] = 0;
+                q.push({i,j});
             }
         }
-        vector<vector<int>> ans(r,vector<int>(c,0));
-        vector<vector<bool>> visited(r,vector<bool>(c,false));
-        vector<vector<int>> directions={{-1,0},{1,0},{0,-1},{0,1}};
-        while(!q.empty()){
-            auto cell=q.front();
-            q.pop();
-            int x=cell.first.first;
-            int y=cell.first.second;
-            int dist=cell.second;
-            visited[x][y]=true;
-            for(vector<int> &d: directions){
-                int nx=x+d[0];
-                int ny=y+d[1];
-                if((nx>=0 && nx<r && ny>=0 && ny<c) && !visited[nx][ny] && mat[nx][ny]==1){
-                    ans[nx][ny]=dist+1;
-                    q.push({{nx,ny},dist+1});
-                    visited[nx][ny]=true;
-                }
-
-
-            }
-        }
-        return ans;
-        
-        
     }
+
+    while(!q.empty()) {
+        auto [x, y] = q.front();
+        q.pop();
+        for(auto &d : directions) {
+            int nx = x + d[0], ny = y + d[1];
+            if(nx >= 0 && nx < r && ny >= 0 && ny < c && ans[nx][ny] == -1) {
+                ans[nx][ny] = ans[x][y] + 1;
+                q.push({nx, ny});
+            }
+        }
+    }
+
+    return ans;
+}
+
 };
