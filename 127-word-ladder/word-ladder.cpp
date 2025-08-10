18247756c352ec8221eb2ad32e94529e9c1922e5
Rohansejwal64 <137511240+Rohansejwal64@users.noreply.github.com>
2025-08-11 00:57:04 +0530

    Time: 50 ms (85.09%) | Memory: 21.1 MB (77.68%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,46 +1,32 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        if(beginWord==endWord) return 0;
-        unordered_set<string> s;
-        for(auto &st:wordList){
-            s.insert(st);
-        }
-         if (s.find(endWord) == s.end()) return 0; // endWord not in list
-        int count=0;
-        bool find=false;
-        queue<pair<string,int>> q;
-        q.push({beginWord,1});
-        count++;
-        if(s.find(beginWord)!=s.end()) {
-            s.erase(beginWord);
-        }
-        while(!q.empty()){
-            
-            auto it =q.front();
-            q.pop();
-            string temp=it.first;
-            if(temp==endWord){
-                count=it.second;
-                find=true;
-                break;
-            }
-            for(int i=0;i<temp.size();i++){
-                for(char ch='a';ch<='z';ch++){
-                    string temp2=temp;
-                    temp2[i]=ch;
-                    if(s.find(temp2)!=s.end()){
-                        q.push({temp2,it.second+1});
-                        s.erase(temp2);
-                    }
+   int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+    unordered_set<string> dict(wordList.begin(), wordList.end());
+
+    if (dict.find(endWord) == dict.end()) return 0; // endWord not in list
+
+    queue<pair<string,int>> q;
+    q.push({beginWord, 1});
+    dict.erase(beginWord);
+
+    while (!q.empty()) {
+        auto [word, steps] = q.front();
+        q.pop();
+
+        if (word == endWord) return steps;
+
+        for (int i = 0; i < word.size(); i++) {
+            string nextWord = word;
+            for (char ch = 'a'; ch <= 'z'; ch++) {
+                nextWord[i] = ch;
+                if (dict.find(nextWord) != dict.end()) {
+                    q.push({nextWord, steps + 1});
+                    dict.erase(nextWord);
                 }
             }
-            
         }
-        return (find)?count:0;
-        
-
-    
-        
     }
+    return 0; // no transformation found
+}
+
 };
