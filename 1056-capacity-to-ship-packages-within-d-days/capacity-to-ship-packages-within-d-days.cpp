63aca14bdcfcf07f2b6e8a7604fd7eb915b44af6
Rohansejwal64 <137511240+Rohansejwal64@users.noreply.github.com>
2025-08-23 23:03:56 +0530

    Time: 16 ms (23.37%) | Memory: 35.1 MB (13.03%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,39 +1,31 @@
 class Solution {
 public:
-int solve(vector<int>& arr, int mid){
-    int count=0;
-    int capa=mid;
-    int days=0;
-    int i=0;
-    while(i<arr.size()){
-        
-        if(arr[i]<=capa){
-            count++;
-            capa-=arr[i++];
-            
+int solve(vector<int>& arr, int mid) {
+    int days = 0;   // at least one day is needed
+    int curr = 0;
+
+    for(int i=0; i<arr.size(); i++) {
+        if(arr[i] > mid) return INT_MAX; // can't fit this package
+        if(curr + arr[i] > mid) {
+            days++;
+            curr = 0;
         }
-        else{
-            
-            
-                days++;
-                count=0;
-                capa=mid;
-            
-        }
+        curr += arr[i];
     }
     return days+1;
-
-
 }
     int shipWithinDays(vector<int>& arr, int days) {
         int n=arr.size();
-        long long int maxi=0;
+        long long int ts=0;
+        int maxi=INT_MIN;
         for(int i=0;i<n;i++){
-            maxi+=arr[i];
+            ts+=arr[i];
+            maxi=max(maxi,arr[i]);
+
 
         }
-        int s= *max_element(arr.begin(),arr.end());
-        long long int e=maxi;
+        int s= maxi;
+        long long int e=ts;
         int ans=-1;
         while(s<=e){
             long long mid=s+(e-s)/2;
