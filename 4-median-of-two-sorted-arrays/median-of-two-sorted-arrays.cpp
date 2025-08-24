770bdddbe61235cd7a07e18de14107f4c81d39b4
Rohansejwal64 <137511240+Rohansejwal64@users.noreply.github.com>
2025-08-25 00:28:30 +0530

    Time: 0 ms (100.00%) | Memory: 95.2 MB (61.65%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> temp;
-        int i=0,j=0;
-        while(i<nums1.size() && j<nums2.size()){
-            if(nums1[i]<=nums2[j]){
-                temp.push_back(nums1[i++]);
-            }
-            else{
-                temp.push_back(nums2[j++]);
+    int s1 = nums1.size(), s2 = nums2.size();
+    int n = s1 + s2;
 
-            }
-        }
-        if(i==nums1.size()){
-            while(j<nums2.size()){
-                temp.push_back(nums2[j++]);
-            }
-        }
-        if(j==nums2.size()){
-            while(i<nums1.size()){
-                temp.push_back(nums1[i++]);
-            }
+    int ind1 = (n % 2 == 0) ? (n / 2) - 1 : -1; // only needed if even
+    int ind2 = n / 2;
 
+    int ind1e = -1, ind2e = -1;
+    int i = 0, j = 0, count = 0;
+
+    while (i < s1 || j < s2) {
+        int val;
+        if (j >= s2 || (i < s1 && nums1[i] <= nums2[j])) {
+            val = nums1[i++];
+        } else {
+            val = nums2[j++];
         }
-        int n=temp.size();
-        if(n%2==0){
-            return double(temp[n/2]+temp[(n/2)-1])/2.0 ;
+
+        if (count == ind1) ind1e = val;
+        if (count == ind2) {
+            ind2e = val;
+            break; // no need to continue
         }
-        else{
-            return double(temp[n/2]);
-        }
-        
+        count++;
     }
+
+    if (n % 2 == 0)
+        return (ind1e + ind2e) / 2.0;
+    else
+        return ind2e;
+}
 };
