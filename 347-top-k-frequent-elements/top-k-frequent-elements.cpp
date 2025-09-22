184b57c41d0bbec69aa61a022c92ba6df6c258e8
Rohansejwal64 <137511240+Rohansejwal64@users.noreply.github.com>
2025-09-22 13:25:57 +0530

    Time: 3 ms (49.60%) | Memory: 19.9 MB (8.33%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -8,24 +8,26 @@ public:
             freq[x]++;
         }
 
-        // Step 2: Store in vector of pairs (element, frequency)
-        vector<pair<int,int>> temp;
-        for(auto &p : freq){
-            temp.push_back({p.first, p.second});
+       vector<vector<int>> bucket(nums.size()+1);
+
+       for( auto temp : freq){
+        int ele= temp.first;
+        int f=temp.second;
+        bucket[f].push_back(ele);
+       }
+       vector<int> ans;
+
+       for(int i=nums.size();i>=0;i--){
+        if(bucket[i].size()==0) continue;
+        while(bucket[i].size()>0 && k>0){
+            ans.push_back(bucket[i].back());
+            bucket[i].pop_back();
+            k--;
         }
+       }
+       return ans;
 
-        // Step 3: Sort by frequency in descending order
-        sort(temp.begin(), temp.end(), [](pair<int,int>& a, pair<int,int>& b){
-            return a.second > b.second; 
-        });
 
-        // Step 4: Take top k elements
-        vector<int> ans;
-        for(int i=0;i<k;i++){
-            ans.push_back(temp[i].first);
-        }
-
-        return ans;
         
     }
 };
