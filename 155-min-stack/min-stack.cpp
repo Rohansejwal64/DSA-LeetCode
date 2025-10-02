b33fbf9aa130d8dc8266eea5a07844aba67e3ab9
Rohansejwal64 <137511240+Rohansejwal64@users.noreply.github.com>
2025-10-03 02:11:34 +0530

    Time: 3 ms (61.20%) | Memory: 23.5 MB (44.91%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,38 +1,53 @@
 class MinStack {
 public:
-stack<int> st;
-stack<int> mst;
+    stack<long long> st;
+    long long minval;
+
     MinStack() {
-        
+        minval = LLONG_MAX;
     }
     
     void push(int val) {
-        st.push(val);
-        if(mst.empty() || val <=mst.top()) mst.push(val);
-        
+        if (st.empty()) {
+            st.push(val);
+            minval = val;
+        }
+        else if (val >= minval) {
+            st.push(val);
+        }
+        else {
+            st.push(2LL * val - minval); // encode
+            minval = val;
+        }
     }
     
     void pop() {
-       
-        if(mst.top()==st.top()){
-            mst.pop();
-
-        }
-        st.pop();
+        if (st.empty()) return;
         
+        if (st.top() >= minval) {
+            st.pop();
+        }
+        else {
+            minval = 2LL * minval - st.top(); // decode
+            st.pop();
+        }
+        
+        if (st.empty()) minval = LLONG_MAX; // reset
     }
     
     int top() {
-        return st.top();
-
-        
+        if (st.empty()) return -1;
+        long long topVal = st.top();
+        if (topVal >= minval) return (int)topVal;
+        else return (int)minval; // decoded
     }
     
     int getMin() {
-        return mst.top();
-        
+        if (st.empty()) return -1; // safety
+        return (int)minval;
     }
 };
+
 
 /**
  * Your MinStack object will be instantiated and called as such:
