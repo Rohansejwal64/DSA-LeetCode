dc3cb7924cc39f2680da552180c3dd56cb3ed3bc
Rohansejwal64 <137511240+Rohansejwal64@users.noreply.github.com>
2025-10-05 00:06:57 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (78.14%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    string reverseWords(string s) {
-        string temp;
-        for (int i = 0; i < s.size(); i++) {
-            if(s[i]!=' ') temp+=s[i];
-            else{
-                if(!temp.empty() && temp.back()!=' ')temp+=s[i];
-            }
-        }
-        if(temp.size()!=0 && temp.back()==' ') temp.pop_back();
-
-        reverse(temp.begin(),temp.end());
-        int st=0;
-        for(int i=0;i<=temp.size();i++){
-            if(temp[i]==' ' || i==temp.size()){
-                reverse(temp.begin()+st, temp.begin()+i);
-                st=i+1;
-            }
-        }
-        return temp;
+   string reverseWords(string s) {
+    // Step 1: Remove extra spaces and trim
+    string temp;
+    for (char c : s) {
+        if (c != ' ') temp += c;
+        else if (!temp.empty() && temp.back() != ' ') temp += ' ';
     }
+    if (!temp.empty() && temp.back() == ' ') temp.pop_back();
+
+    // Step 2: Reverse the entire string
+    reverse(temp.begin(), temp.end());
+
+    // Step 3: Reverse each word
+    int start = 0;
+    for (int i = 0; i <= temp.size(); i++) {
+        if (i == temp.size() || temp[i] == ' ') {
+            reverse(temp.begin() + start, temp.begin() + i);
+            start = i + 1;
+        }
+    }
+
+    return temp;
+}
 };
